1b181ba67b4b9e2ed3d49f58b89094344a1c81eb
Rabiul Hasan <rhrobi2015@gmail.com>
2024-10-12 22:02:21 +0600

    url

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.json())
 export const connect = async () => {
     try {
       mongoose.set('strictQuery', false)
-      await mongoose.connect(configEnv.database.DB_URI, {
+      await mongoose.connect(process.env.DB_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true
       })
