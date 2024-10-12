205569a027d39c0cf6242c38cf84af263f7308ca
Rabiul Hasan <rhrobi2015@gmail.com>
2024-10-12 22:08:00 +0600

    url

--- server.js
+++ server.js
@@ -19,10 +19,7 @@ app.use(express.json())
 export const connect = async () => {
     try {
       mongoose.set('strictQuery', false)
-      await mongoose.connect(process.env.DB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-      })
+      await mongoose.connect(process.env.DB_URI)
       console.log('Connected MongoDB!')
     } catch (error) {
       throw error
